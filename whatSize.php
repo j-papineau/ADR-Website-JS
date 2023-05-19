27838db993269bf62d58dddc809a5229d079f62f
Joel Papineau <118376438+j-papineau@users.noreply.github.com>
2023-05-19 13:51:36 -0400

    whatSize php update hidden Div
    needs default "no id" case

--- whatSize.php
+++ whatSize.php
@@ -5,7 +5,21 @@
 	//require_once('../../../wp-config.php');
 	global $wpdb;
 
-	$results = $wpdb->get_results("select * from Location_Data_Test Where id = '1'");
+	//$results = $wpdb->get_results("select * from Location_Data_Test Where id = '2'");
+	
+
+	//get id from ?id=foo in URL, default is 1
+
+	if(isset($_GET['id'])){
+		$id = $_GET['id'];
+	}else{
+		$id = "1";
+	}
+	
+
+	$results = $wpdb->get_results('select * from Location_Data_Test Where id = '.$id);
+
+
 	//echo $results[0]->name_long;
 	$id = $results[0]->id;
 	$slug = $results[0]->location_slug;
@@ -14,21 +28,29 @@
 	$phone = $results[0]->phone;
 	//$phone = "test"
 	
-	$id = $_SERVER['QUERY_STRING'];
+	//$id = parse_url($url, PHP_URL_QUERY);
+
+	if(isset($_GET['id'])){
+		$id = $_GET['id'];
+	}else{
+		$id = "1";
+	}
 	
 
 
 ?>
 
-<div id="phone">
+<div id="values">
 	
 	
 		<?php
-		echo $phone;
+		echo $phone, "+", $slug,"+",$longName,"+",$shortName;
 		?>
 	
 		<?php
-		echo $id;
+	
+		echo $id
+		//echo $params;
 		?>
 	
 </div>
